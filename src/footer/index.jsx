5b1d7701f3f8587a144d25788510540fbe5ef9fb
Usiel-03A7 <shebastyanmail.com>
2023-04-16 11:42:42 -0600

    the new image

---
+++ src/footer/index.jsx
@@ -0,0 +1,10 @@
+import './footer.css'
+export default function Footer (){
+    return (
+        <>
+        <div className="futter">
+            <h3>Workin in this page</h3>
+        </div>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import Nav from './navbar'
 import './index.css'
 import Inicio from './inicio'
 import Proyectos from './proyectos'
+import Footer from './footer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <Nav />
     <Inicio />
     <Proyectos />
+    <Footer />
   </>
 )
